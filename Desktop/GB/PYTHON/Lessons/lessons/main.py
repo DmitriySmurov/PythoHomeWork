f3576fc822e06ef64b50dbfdc6a6eedcc6465f92
Dmitriy <dsmurov@mail.ru>
2022-11-26 17:17:21 +0300

    8, lesson main

--- Desktop/GB/PYTHON/Lessons/lessons/main.py
+++ Desktop/GB/PYTHON/Lessons/lessons/main.py
@@ -17,16 +17,16 @@
 
 
 
-from os.path import exists
-from CSV_creating import creating
-from File_writing import writing_scv
-from File_writing import writing_txt
+# from os.path import exists
+# from CSV_creating import creating
+# from File_writing import writing_scv
+# from File_writing import writing_txt
 
 
-path = 'Phonebook.csv'
-valid = exists(path)
-if not valid:
-    creating()
+# path = 'Phonebook.csv'
+# valid = exists(path)
+# if not valid:
+#     creating()
 
-writing_scv()
-writing_txt()
+# writing_scv()
+# writing_txt()
